ffcd16571b3b24e183e42858043a42824a0363ea
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-06-05 00:33:10 +0300

    Refactoring

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import apiRoutes from "./api/routes/api.js";
-import db from "./db/knex.js";
 import cron from "node-cron";
 import Mailer from "./api/managers/Mailer.js";
 
